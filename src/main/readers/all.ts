3516ba9ced4f405a11beb62e4c2ccb891ee488b1
smikhalevski <smikhalevski@gmail.com>
2022-05-03 14:13:13 +0300

    Added unrollingCount option to AllCharCodeRangeReader

--- src/main/readers/all.ts
+++ src/main/readers/all.ts
@@ -20,6 +20,14 @@ export interface AllOptions {
    * @default 0
    */
   maximumCount?: number;
+
+  /**
+   * The number of iterations [to unroll from a loop](https://en.wikipedia.org/wiki/Loop_unrolling) when
+   * {@link maximumCount} is omitted.
+   *
+   * @default 5
+   */
+  unrollingCount?: number;
 }
 
 /**
@@ -35,62 +43,72 @@ export function all<Context = any>(reader: Reader<Context>, options: AllOptions 
   let {
     minimumCount = 0,
     maximumCount = 0,
+    unrollingCount = 5,
   } = options;
 
   minimumCount = Math.max(minimumCount | 0, 0);
   maximumCount = Math.max(maximumCount | 0, 0); // 0 = Infinity
+  unrollingCount = Math.max(unrollingCount | 0, 1);
 
-  if (maximumCount > 0 && minimumCount > maximumCount) {
+  if (maximumCount && minimumCount > maximumCount) {
     return never;
   }
-  if (minimumCount === 1 && maximumCount === 1) {
-    return reader;
-  }
-  if (reader === never || reader === none) {
+  if (minimumCount === 1 && maximumCount === 1 || reader === never || reader === none) {
     return reader;
   }
   if (reader instanceof CharCodeRangeReader) {
-    return new AllCharCodeRangeReader(reader.charCodeRanges, minimumCount, maximumCount);
+    return new AllCharCodeRangeReader(reader.charCodeRanges, minimumCount, maximumCount, unrollingCount);
   }
-  return new AllReader(reader, minimumCount, maximumCount);
+  return new AllReader(reader, minimumCount, maximumCount, unrollingCount);
 }
 
 export class AllCharCodeRangeReader implements ReaderCodegen {
 
-  constructor(public charCodeRanges: CharCodeRange[], public minimumCount: number, public maximumCount: number) {
+  constructor(public charCodeRanges: CharCodeRange[], public minimumCount: number, public maximumCount: number, public unrollingCount: number) {
   }
 
   factory(inputVar: Var, offsetVar: Var, contextVar: Var, resultVar: Var): CodeBindings {
-    const {charCodeRanges, minimumCount, maximumCount} = this;
+    const {charCodeRanges, minimumCount, maximumCount, unrollingCount} = this;
 
+    const inputLengthVar = createVar();
     const charCodeVar = createVar();
-    const indexVar = createVar();
-
-    const unwoundCount = maximumCount || Math.max(minimumCount, 10);
 
     const code: Code[] = [
-      'var ', charCodeVar, ',',
-      indexVar, '=', offsetVar, ';',
-      resultVar, '=', minimumCount ? NO_MATCH : indexVar, ';',
+      'var ',
+      inputLengthVar, '=', inputVar, '.length,',
+      charCodeVar, ';',
     ];
 
-    for (let i = 0; i < unwoundCount; ++i) {
+    if (minimumCount !== 0) {
+      // Check the required minimum of chars before proceeding
+      // if (offset + minimumCount < input.length && (charCode = input.charCodeAt(offset + i), charPredicate(charCode)) && … )
       code.push(
-          charCodeVar, '=', inputVar, '.charCodeAt(', minimumCount ? indexVar : resultVar, ');',
-          'if(', createCharPredicateCode(charCodeVar, charCodeRanges), '){++', minimumCount ? indexVar : resultVar, ';',
-          minimumCount && i >= minimumCount - 1 ? [resultVar, '=', indexVar, ';'] : '',
+          resultVar, '=', NO_MATCH, ';',
+          'if(', offsetVar, '+', minimumCount - 1, '<', inputLengthVar,
       );
+      for (let i = 0; i < minimumCount; ++i) {
+        code.push('&&(', charCodeVar, '=', inputVar, '.charCodeAt(', offsetVar, '+', i, '),', createCharPredicateCode(charCodeVar, charCodeRanges), ')');
+      }
+      code.push('){', resultVar, '=', offsetVar, '+', minimumCount, ';');
+    } else {
+      code.push(resultVar, '=', offsetVar, ';');
     }
-    if (!maximumCount) {
-      code.push(
-          charCodeVar, '=', inputVar, '.charCodeAt(', resultVar, ');',
-          'while(', createCharPredicateCode(charCodeVar, charCodeRanges), '){',
-          '++', resultVar, ';',
-          charCodeVar, '=', inputVar, '.charCodeAt(', resultVar, ');',
-          '}',
-      );
+
+    const remainingCount = Math.max(0, maximumCount - minimumCount);
+
+    if (maximumCount === 0 || remainingCount !== 0) {
+      // Loop is removed if maximum count is known beforehand
+      // (result < input.length && (charCode = input.charCodeAt(result), charPredicate(charCode)) && ++result < input.length && …) ++result;
+      code.push(maximumCount !== 0 ? 'if(' : 'while(');
+      for (let i = 0; i < (remainingCount || unrollingCount); ++i) {
+        code.push(i === 0 ? '' : '&&++', resultVar, '<', inputLengthVar, '&&(', charCodeVar, '=', inputVar, '.charCodeAt(', resultVar, '),', createCharPredicateCode(charCodeVar, charCodeRanges), ')');
+      }
+      code.push(')++', resultVar, ';');
     }
-    code.push('}'.repeat(unwoundCount));
+
+    if (minimumCount !== 0) {
+      code.push('}');
+    }
 
     return createCodeBindings(code);
   }
@@ -98,7 +116,7 @@ export class AllCharCodeRangeReader implements ReaderCodegen {
 
 export class AllReader<Context> implements ReaderCodegen {
 
-  constructor(public reader: Reader<Context>, public minimumCount: number, public maximumCount: number) {
+  constructor(public reader: Reader<Context>, public minimumCount: number, public maximumCount: number, public unrollingCount: number) {
   }
 
   factory(inputVar: Var, offsetVar: Var, contextVar: Var, resultVar: Var): CodeBindings {
@@ -107,30 +125,64 @@ export class AllReader<Context> implements ReaderCodegen {
     const bindings: Binding[] = [];
     const indexVar = createVar();
     const readerResultVar = createVar();
-    const readCountVar = createVar();
 
-    return createCodeBindings(
-        [
-          'var ',
-          indexVar, ',',
-          readerResultVar, '=', offsetVar,
-          minimumCount || maximumCount ? [',', readCountVar, '=0'] : '',
-          ';',
-          'do{',
+    const unwoundCount = maximumCount || Math.max(minimumCount, 10);
 
-          // Ensure that we actually use a numeric result
-          indexVar, '=', readerResultVar, '/1;',
+    const code: Code[] = [
+      'var ', readerResultVar, ',',
+      indexVar, '=', offsetVar, ';',
+      resultVar, '=', minimumCount ? NO_MATCH : indexVar, ';',
+    ];
+
+    for (let i = 0; i < unwoundCount; ++i) {
+      code.push(
           createReaderCallCode(reader, inputVar, indexVar, contextVar, readerResultVar, bindings),
-          '}while(',
-          readerResultVar, '>', indexVar,
-          minimumCount || maximumCount ? ['&&++', readCountVar, maximumCount ? '<' + maximumCount : ''] : '',
-          ')',
-          resultVar, '=',
-          minimumCount ? [readCountVar, '<', minimumCount, '?', NO_MATCH, ':'] : '',
-          readerResultVar, '===', NO_MATCH, '?', indexVar, ':', readerResultVar,
-          ';',
-        ],
-        bindings,
-    );
+          'if(', readerResultVar, '>', indexVar, '){',
+          indexVar, '=', readerResultVar, ';',
+          !minimumCount || i >= minimumCount - 1 ? [resultVar, '=', indexVar, ';'] : '',
+      );
+    }
+    if (!maximumCount) {
+      code.push(
+          createReaderCallCode(reader, inputVar, resultVar, contextVar, readerResultVar, bindings),
+          'while(', readerResultVar, '>', resultVar, '){',
+          resultVar, '=', readerResultVar, ';',
+          '}',
+      );
+    }
+    code.push('}'.repeat(unwoundCount));
+
+    return createCodeBindings(code, bindings);
+
+    // const {reader, minimumCount, maximumCount} = this;
+    //
+    // const bindings: Binding[] = [];
+    // const indexVar = createVar();
+    // const readerResultVar = createVar();
+    // const readCountVar = createVar();
+    //
+    // return createCodeBindings(
+    //     [
+    //       'var ',
+    //       indexVar, ',',
+    //       readerResultVar, '=', offsetVar,
+    //       minimumCount || maximumCount ? [',', readCountVar, '=0'] : '',
+    //       ';',
+    //       'do{',
+    //
+    //       // Ensure that we actually use a numeric result
+    //       indexVar, '=', readerResultVar, '/1;',
+    //       createReaderCallCode(reader, inputVar, indexVar, contextVar, readerResultVar, bindings),
+    //       '}while(',
+    //       readerResultVar, '>', indexVar,
+    //       minimumCount || maximumCount ? ['&&++', readCountVar, maximumCount ? '<' + maximumCount : ''] : '',
+    //       ')',
+    //       resultVar, '=',
+    //       minimumCount ? [readCountVar, '<', minimumCount, '?', NO_MATCH, ':'] : '',
+    //       readerResultVar, '===', NO_MATCH, '?', indexVar, ':', readerResultVar,
+    //       ';',
+    //     ],
+    //     bindings,
+    // );
   }
 }
--- src/test/readers/all.test.ts
+++ src/test/readers/all.test.ts
@@ -40,50 +40,50 @@ describe('all', () => {
 
 describe('AllCharCodeRangeReader', () => {
 
-  test('reads exact number of chars', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2))('aaaa', 0)).toBe(2);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2))('aaaa', 1)).toBe(3);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2))('aaaa', 2)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2))('abbb', 0)).toBe(NO_MATCH);
+  test('reads the exact number of chars', () => {
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2, 2))('aaaa', 0)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2, 2))('aaaa', 1)).toBe(3);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2, 2))('aaaa', 2)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2, 2))('abbb', 0)).toBe(NO_MATCH);
   });
 
-  test('reads exact number of chars when length is insufficient', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2))('aaaa', 3)).toBe(NO_MATCH);
+  test('reads the exact number of chars when length is insufficient', () => {
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 2, 2, 2))('aaaa', 3)).toBe(NO_MATCH);
   });
 
   test('reads maximum number of chars', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2))('aaaa', 0)).toBe(2);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2))('aaaa', 1)).toBe(3);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2))('abbb', 0)).toBe(1);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2, 2))('aaaa', 0)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2, 2))('aaaa', 1)).toBe(3);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2, 2))('abbb', 0)).toBe(1);
   });
 
   test('reads maximum number of chars does not overflow input length', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2))('aaaa', 3)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 2, 2))('aaaa', 3)).toBe(4);
   });
 
   test('reads minimum and maximum number of chars', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2))('aaaa', 0)).toBe(2);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2))('aaaa', 1)).toBe(3);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2))('aaaa', 3)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2))('aaaa', 4)).toBe(NO_MATCH);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2))('aabb', 1)).toBe(2);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2))('aabb', 2)).toBe(NO_MATCH);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2, 2))('aaaa', 0)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2, 2))('aaaa', 1)).toBe(3);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2, 2))('aaaa', 3)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2, 2))('aaaa', 4)).toBe(NO_MATCH);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2, 2))('aabb', 1)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 2, 2))('aabb', 2)).toBe(NO_MATCH);
   });
 
   test('reads minimum number of chars', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0))('aaaa', 0)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0))('aaaa', 1)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0))('aaaa', 4)).toBe(NO_MATCH);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0))('aabb', 1)).toBe(2);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0))('aabb', 2)).toBe(NO_MATCH);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0, 2))('aaaa', 0)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0, 2))('aaaa', 1)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0, 2))('aaaa', 4)).toBe(NO_MATCH);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0, 2))('aabb', 1)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 1, 0, 2))('aabb', 2)).toBe(NO_MATCH);
   });
 
   test('reads unlimited number of chars', () => {
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0))('aaaa', 0)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0))('aaaa', 1)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0))('aaaa', 4)).toBe(4);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0))('aabb', 1)).toBe(2);
-    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0))('aabb', 2)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0, 2))('aaaa', 0)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0, 2))('aaaa', 1)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0, 2))('aaaa', 4)).toBe(4);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0, 2))('aabb', 1)).toBe(2);
+    expect(toReaderFunction(new AllCharCodeRangeReader([A], 0, 0, 2))('aabb', 2)).toBe(2);
   });
 });
 
@@ -95,7 +95,7 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(4);
     readerMock.mockReturnValueOnce(NO_MATCH);
 
-    expect(toReaderFunction(new AllReader(readerMock, 0, 0))('aabbcc', 2)).toBe(4);
+    expect(toReaderFunction(new AllReader(readerMock, 0, 0, 2))('aabbcc', 2)).toBe(4);
     expect(readerMock).toHaveBeenCalledTimes(3);
   });
 
@@ -104,7 +104,7 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(3);
     readerMock.mockReturnValueOnce(3);
 
-    expect(toReaderFunction(new AllReader(readerMock, 0, 0))('aabbcc', 2)).toBe(3);
+    expect(toReaderFunction(new AllReader(readerMock, 0, 0, 2))('aabbcc', 2)).toBe(3);
     expect(readerMock).toHaveBeenCalledTimes(2);
   });
 
@@ -114,7 +114,7 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(-2);
     readerMock.mockReturnValueOnce(3);
 
-    expect(toReaderFunction(new AllReader(readerMock, 0, 0))('aabbcc', 2)).toBe(-2);
+    expect(toReaderFunction(new AllReader(readerMock, 0, 0, 2))('aabbcc', 2)).toBe(-2);
     expect(readerMock).toHaveBeenCalledTimes(2);
   });
 
@@ -123,7 +123,7 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(1);
     readerMock.mockReturnValueOnce(NO_MATCH);
 
-    expect(toReaderFunction(new AllReader(readerMock, 2, 0))('a', 0)).toBe(NO_MATCH);
+    expect(toReaderFunction(new AllReader(readerMock, 2, 0, 2))('a', 0)).toBe(NO_MATCH);
     expect(readerMock).toHaveBeenCalledTimes(2);
   });
 
@@ -134,7 +134,7 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(3);
     readerMock.mockReturnValueOnce(NO_MATCH);
 
-    expect(toReaderFunction(new AllReader(readerMock, 2, 0))('aaa', 0)).toBe(3);
+    expect(toReaderFunction(new AllReader(readerMock, 2, 0, 2))('aaa', 0)).toBe(3);
     expect(readerMock).toHaveBeenCalledTimes(4);
   });
 
@@ -144,7 +144,7 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(2);
     readerMock.mockReturnValueOnce(3);
 
-    expect(toReaderFunction(new AllReader(readerMock, 0, 2))('aaa', 0)).toBe(2);
+    expect(toReaderFunction(new AllReader(readerMock, 0, 2, 2))('aaa', 0)).toBe(2);
     expect(readerMock).toHaveBeenCalledTimes(2);
   });
 
@@ -153,19 +153,19 @@ describe('AllReader', () => {
     readerMock.mockReturnValueOnce(1);
     readerMock.mockReturnValueOnce(NO_MATCH);
 
-    expect(toReaderFunction(new AllReader(readerMock, 0, 2))('a', 0)).toBe(1);
+    expect(toReaderFunction(new AllReader(readerMock, 0, 2, 2))('a', 0)).toBe(1);
     expect(readerMock).toHaveBeenCalledTimes(2);
   });
 
   test('can use inline readers', () => {
-    expect(toReaderFunction(new AllReader(text('a'), 0, 0))('aabbcc', 0)).toBe(2);
+    expect(toReaderFunction(new AllReader(text('a'), 0, 0, 2))('aabbcc', 0)).toBe(2);
   });
 
   test('propagates context', () => {
     const readerMock = jest.fn(() => 0);
     const context = Symbol('context');
 
-    expect(toReaderFunction<any>(new AllReader(readerMock, 0, 0))('a', 0, context)).toBe(0);
+    expect(toReaderFunction<any>(new AllReader(readerMock, 0, 0, 2))('a', 0, context)).toBe(0);
 
     expect(readerMock).toHaveBeenCalledTimes(1);
     expect(readerMock).toHaveBeenNthCalledWith(1, 'a', 0, context);
